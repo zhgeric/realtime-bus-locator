5243f51c0a89231488c941b6e01f4fcb0f165931
zhgeric <zhang.eric.f@gmail.com>
2024-05-20 12:50:30 +0200

    test server using node

--- driver-app/locate-my-bus/App.js
+++ driver-app/locate-my-bus/App.js
@@ -1,132 +1,180 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, Pressable } from 'react-native';
-import * as Location from 'expo-location';
-import { Picker } from '@react-native-picker/picker';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Button, Text, Pressable } from "react-native";
+import * as Location from "expo-location";
+import { Picker } from "@react-native-picker/picker";
+import { io } from "socket.io-client";
+
+export const socket = io("http://192.168.1.20:3000");
 
 export default function App() {
-  const [isTracking, setIsTracking] = useState(false);
-  const [route, setRoute] = useState(null);
-  const [selectedBus, setSelectedBus] = useState(null);
-  const [selectedDirection, setSelectedDirection] = useState(null);
+	const [isTracking, setIsTracking] = useState(false);
+	const [route, setRoute] = useState(null);
+	const [selectedBus, setSelectedBus] = useState(null);
+	const [selectedDirection, setSelectedDirection] = useState(null);
 
-  const buses = {
-    "128": ["Porte d'Orléans", "Robinson RER"],
-    "188": ["Bagneux - Rosenberg", "Porte d'Orléans"],
-    "388": ["Bourg-la-Reine RER", "Porte d'Orléans"],
-  };
+	const [isConnected, setIsConnected] = useState(false);
+	const [transport, setTransport] = useState("N/A");
 
-  const startTracking = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.error('Permission denied');
-      return;
-    }
+	useEffect(() => {
+		if (socket.connected) {
+			onConnect();
+		}
 
-    const sendLocation=(location, busNumber, direction)=> {
-      fetch('http://localhost:3000/location', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          location: {
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          },
-          bus:{
-            line_number: busNumber,
-            direction: direction,
-          }
-        }),
-      });
-    }
-    const destination = await Location.watchPositionAsync({
-      accuracy: Location.Accuracy.High,
-      timeInterval: 500,
-      distanceInterval: 1,
-    }, (location) => {
-      console.log(location);
-      sendLocation(location, selectedBus, selectedDirection);
-    });
-    setRoute(destination);
-    setIsTracking(true);
-  };
+		function onConnect() {
+			setIsConnected(true);
+			setTransport(socket.io.engine.transport.name);
 
-  const stopTracking = () => {
-    if (route) {
-      route.remove();
-      setRoute(null);
-    }
-    setIsTracking(false);
-  };
+			socket.io.engine.on("upgrade", (transport) => {
+				setTransport(transport.name);
+			});
+		}
 
-  return (
-    <View style={styles.container}>
-      <Picker
-        enabled={!isTracking}
-        style={styles.picker}
-        selectedValue={selectedBus}
-        onValueChange={(itemValue) => setSelectedBus(itemValue)}
-        >
-        <Picker.Item label="Select a bus" value={null} />
-        {Object.keys(buses).map((bus) => (
-          <Picker.Item key={bus} label={bus} value={bus} />
-        ))}
-      </Picker>
+		function onDisconnect() {
+			setIsConnected(false);
+			setTransport("N/A");
+		}
 
-      {selectedBus && (
-        <Picker
-          enabled={!isTracking}
-          style={styles.picker}
-          selectedValue={selectedDirection}
-          onValueChange={(itemValue) => setSelectedDirection(itemValue)}
-          >
-          <Picker.Item label="Select a direction" value={null} />
-          {buses[selectedBus].map((direction, index) => (
-            <Picker.Item key={index} label={direction} value={direction} />
-          ))}
-        </Picker>
-      )}
+		socket.on("connect", onConnect);
+		socket.on("disconnect", onDisconnect);
 
-      {isTracking ? (
-        <Pressable onPress={stopTracking} style={styles.stopButton}>
-          <Text style={styles.buttonText}>Stop Tracking</Text>
-        </Pressable>
-      ) : (
-        <Pressable onPress={startTracking} style={styles.button} disabled={!selectedDirection}>
-          <Text style={styles.buttonText}>Start Tracking</Text>
-        </Pressable>
-      )}
-    </View>
-  );
+		return () => {
+			socket.off("connect", onConnect);
+			socket.off("disconnect", onDisconnect);
+		};
+	}, []);
+
+	const buses = {
+		128: ["Porte d'Orléans", "Robinson RER"],
+		188: ["Bagneux - Rosenberg", "Porte d'Orléans"],
+		388: ["Bourg-la-Reine RER", "Porte d'Orléans"],
+	};
+
+	const startTracking = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			console.error("Permission denied");
+			return;
+		}
+
+		const sendLocation = (location, busNumber, direction) => {
+			const currentLocation = JSON.stringify({
+				location: {
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude,
+				},
+				bus: {
+					line_number: busNumber,
+					direction: direction,
+				},
+			});
+      socket.emit('chat message', currentLocation)
+		};
+
+		const destination = await Location.watchPositionAsync(
+			{
+				accuracy: Location.Accuracy.High,
+				timeInterval: 500,
+				distanceInterval: 1,
+			},
+			(location) => {
+				sendLocation(location, selectedBus, selectedDirection);
+			}
+		);
+		setRoute(destination);
+		setIsTracking(true);
+	};
+
+	const stopTracking = () => {
+		if (route) {
+			route.remove();
+			setRoute(null);
+		}
+		setIsTracking(false);
+	};
+
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text>
+					Status: {isConnected ? "connected" : "disconnected"}
+				</Text>
+				<Text>Transport: {transport}</Text>
+			</View>
+			<Picker
+				enabled={!isTracking}
+				style={styles.picker}
+				selectedValue={selectedBus}
+				onValueChange={(itemValue) => setSelectedBus(itemValue)}
+			>
+				<Picker.Item label="Select a bus" value={null} />
+				{Object.keys(buses).map((bus) => (
+					<Picker.Item key={bus} label={bus} value={bus} />
+				))}
+			</Picker>
+
+			{selectedBus && (
+				<Picker
+					enabled={!isTracking}
+					style={styles.picker}
+					selectedValue={selectedDirection}
+					onValueChange={(itemValue) =>
+						setSelectedDirection(itemValue)
+					}
+				>
+					<Picker.Item label="Select a direction" value={null} />
+					{buses[selectedBus].map((direction, index) => (
+						<Picker.Item
+							key={index}
+							label={direction}
+							value={direction}
+						/>
+					))}
+				</Picker>
+			)}
+
+			{isTracking ? (
+				<Pressable onPress={stopTracking} style={styles.stopButton}>
+					<Text style={styles.buttonText}>Stop Tracking</Text>
+				</Pressable>
+			) : (
+				<Pressable
+					onPress={startTracking}
+					style={styles.button}
+					disabled={!selectedDirection}
+				>
+					<Text style={styles.buttonText}>Start Tracking</Text>
+				</Pressable>
+			)}
+		</View>
+	);
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-  },
-  picker: {
-    marginBottom: 20,
-    height: 50,
-  },
-  button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  stopButton: {
-    backgroundColor: 'red',
-    padding: 10,
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-  },
-  buttonDisabled: {
-    opacity: 0.5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		padding: 20,
+	},
+	picker: {
+		marginBottom: 20,
+		height: 50,
+	},
+	button: {
+		backgroundColor: "#007BFF",
+		padding: 10,
+		alignItems: "center",
+		borderRadius: 5,
+	},
+	stopButton: {
+		backgroundColor: "red",
+		padding: 10,
+		alignItems: "center",
+		borderRadius: 5,
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 16,
+	},
+	buttonDisabled: {
+		opacity: 0.5,
+	},
 });
