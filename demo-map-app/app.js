469aaa39f8c929b7fad388aa552455cec01cc400
zhgeric <zhang.eric.f@gmail.com>
2024-05-21 18:12:46 +0200

    feature: change popup style

--- demo-map-app/app.js
+++ demo-map-app/app.js
@@ -1,12 +1,12 @@
 // Créer la carte
 import { io } from "socket.io-client";
-import geojson from "./stationspre.json" with { type: "json" };
+import geojson from "./stationspre.json" assert { type: "json" };
 
 const socket = io("http://localhost:3000");
 
-var map = L.map("map").setView([48.8066, 2.3022], 14);
+let map = L.map("map").setView([48.8066, 2.3022], 14);
 
-var busIcon = L.Icon.extend({
+let busIcon = L.Icon.extend({
 	options: {
 		shadowUrl: "./busIcon/bus.png",
 		iconSize: [40, 50],
@@ -15,11 +15,18 @@ var busIcon = L.Icon.extend({
 	},
 });
 
-var icons = {
+let icons = {
 	128: new busIcon({ iconUrl: "./busIcon/128bus.png" }),
 	188: new busIcon({ iconUrl: "./busIcon/188bus.png" }),
 	388: new busIcon({ iconUrl: "./busIcon/388bus.png" }),
 	391: new busIcon({ iconUrl: "./busIcon/391bus.png" }),
+};
+
+let busImgs = {
+	128: "./busIcon/128bus.png",
+	188: "./busIcon/188bus.png",
+	388: "./busIcon/388bus.png",
+	391: "./busIcon/391bus.png",
 };
 
 // Ajouter la couche de tuiles OpenStreetMap à la carte
@@ -28,22 +35,26 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 // Charger le fichier GeoJSON et ajouter les marqueurs à la carte
-var busMarker;
+let busMarker;
 L.geoJSON(geojson, {
 	onEachFeature: function (feature, layer) {
 		const div = document.createElement("div");
-		div.innerHTML = `${
+		const bus_station_name = document.createElement("p");
+		bus_station_name.innerHTML = `${
 			(feature.properties.name && feature.properties.name) ||
 			(feature.properties.tags && feature.properties.tags.name)
 		}<br>`;
-
+		bus_station_name.classList.add("bus-station__name");
+		div.append(bus_station_name);
 		if (feature.busses) {
 			feature.busses.forEach(function (bus) {
 				const busDiv = document.createElement("div");
+				busDiv.classList.add("bus-direction")
 				const button = document.createElement("button");
-				button.innerHTML = bus.line_number;
+				button.innerHTML = `<img height="25" src="${busImgs[bus.line_number]}"/>${bus.line_number}`;
+				button.classList.add("bus-button")
 
-				button.onclick = function () {
+				busDiv.onclick = function () {
 					const channel = (bus.direction + bus.line_number)
 						.toLowerCase()
 						.replace(" ", "_");
@@ -61,13 +72,15 @@ L.geoJSON(geojson, {
 							{
 								icon: icons[bus.line_number],
 							}
-						).addTo(map).on("click", () => track = true);
+						)
+							.addTo(map)
+							.on("click", () => (track = true));
 						centerOnMarker(busMarker);
 					});
 				};
 
 				const direction = document.createElement("div");
-				direction.innerHTML = `Direction : ${bus.direction}`;
+				direction.innerHTML = bus.direction;
 
 				busDiv.appendChild(button);
 				busDiv.appendChild(direction);
@@ -82,9 +95,8 @@ L.geoJSON(geojson, {
 let track = false;
 
 const centerOnMarker = (busMark) => {
-	if(track)
-		map.setView(busMark.getLatLng())
-}
+	if (track) map.setView(busMark.getLatLng());
+};
 
 $(document.body).on("mousedown", (event) => {
 	track = false;
